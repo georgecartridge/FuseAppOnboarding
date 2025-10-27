b84d525ade60709be2e5df499b23cc6912354267
George Cartridge <35803468+georgecartridge@users.noreply.github.com>
2025-10-27 20:12:50 +0100

    Initial Commit

---
+++ FuseAppOnboarding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FuseAppOnboarding
+//
+//  Created by George on 27/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FuseAppOnboarding/FuseAppOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuseAppOnboardingApp.swift
+//  FuseAppOnboarding
+//
+//  Created by George on 27/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct FuseAppOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
